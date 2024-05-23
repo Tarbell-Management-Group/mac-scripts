0cabdf25b34767afa214647900506fb50bc585ea
Brian Gates <153768976+bgatestmg@users.noreply.github.com>
2024-05-23 14:27:20 -0400

    Update name.sh

--- name.sh
+++ name.sh
@@ -7,4 +7,13 @@ sudo scutil --set HostName "TMG-$SERIAL_NUMBER"
 sudo scutil --set LocalHostName "TMG-$SERIAL_NUMBER"
 sudo scutil --set ComputerName "TMG-$SERIAL_NUMBER"
 
+# Get a list of all network services
+NETWORK_SERVICES=$(networksetup -listallnetworkservices | tail -n +2)
+
+# Set the search domain for each network service
+for SERVICE in $NETWORK_SERVICES; do
+    sudo networksetup -setsearchdomains "$SERVICE" "bearsden.local"
+done
+
 echo "Device name set to TMG-$SERIAL_NUMBER"
+echo "Search domain set to bearsden.local for all network services"
